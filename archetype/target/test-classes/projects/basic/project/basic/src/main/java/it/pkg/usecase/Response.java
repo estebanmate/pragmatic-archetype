c5db06875aaf23994036034bf63551beddac98ff
Salmane BAH <truegforreal@gmail.com>
2019-09-19 13:10:29 +0200

    Add Docker for generated application

--- archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/usecase/Response.java
+++
@@ -1,4 +0,0 @@
-package it.pkg.usecase;
-
-public interface Response {
-}
--- archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/usecase/UseCase.java
+++
@@ -1,9 +0,0 @@
-package it.pkg.usecase;
-
-@FunctionalInterface
-public interface UseCase {
-    Void nullRequest = null;
-    Void nullResponse = null;
-
-    Response execute(final Request request);
-}
