c05906047f7688e39915ea3527d4761b2ba98825
Salmane BAH <truegforreal@gmail.com>
2020-03-25 10:07:48 +0100

    Add exception directory for usecases

---
+++ archetype/src/main/resources/archetype-resources/src/main/java/usecase/exception/UseCaseException.java
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.usecase.exception;
+
+public class UseCaseException extends RuntimeException {
+}
