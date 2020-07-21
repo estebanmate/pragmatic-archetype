65de810270b94605af7044d9b42b0f7701bc3f76
Salmane BAH <truegforreal@gmail.com>
2020-07-21 22:26:40 +0200

    Remove archetype directory

--- archetype/src/main/resources/archetype-resources/src/main/java/usecase/Request.java
+++
@@ -1,7 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.usecase;
-
-public interface Request {
-}
--- archetype/src/main/resources/archetype-resources/src/main/java/usecase/exception/UseCaseException.java
+++
@@ -1,7 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.usecase.exception;
-
-public class UseCaseException extends RuntimeException {
-}
